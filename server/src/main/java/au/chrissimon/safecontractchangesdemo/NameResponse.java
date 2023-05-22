fae91f0acb4e13c02782226a3f6367b21c7ae056
Chris Simon <chris@devcycles.io>
2023-05-22 15:42:13 +0000

    API - Step 3 - CONTRACT - Server removes backwards compatibility

--- server/src/main/java/au/chrissimon/safecontractchangesdemo/NameResponse.java
+++ server/src/main/java/au/chrissimon/safecontractchangesdemo/NameResponse.java
@@ -3,7 +3,6 @@ package au.chrissimon.safecontractchangesdemo;
 import java.util.UUID;
 
 public class NameResponse {
-    private String name;
     private FullNameDto fullName;
     public FullNameDto getFullName() {
         return fullName;
@@ -23,24 +22,13 @@ public class NameResponse {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public NameResponse() {
         super();
     }
 
-    public NameResponse(UUID id, String name) {
+    public NameResponse(UUID id, FullNameDto fullName) {
         super();
         this.id = id;
-        this.name = name;
-        String[] names = name.split("\\s", 2);
-        String lastName = names.length > 1 ? names[1] : "";
-        this.fullName = new FullNameDto(names[0], lastName);
+        this.fullName = fullName;
     }
 }
--- server/src/test/java/au/chrissimon/safecontractchangesdemo/NameTests.java
+++ server/src/test/java/au/chrissimon/safecontractchangesdemo/NameTests.java
@@ -18,55 +18,6 @@ public class NameTests {
 
     @Value(value="${local.server.port}")
     private int port;
-    
-    @Test
-	public void AddPerson() {
-		WebTestClient client = WebTestClient
-			.bindToServer()
-				.baseUrl("http://localhost:" + port)
-				.build();
-
-        String name = UUID.randomUUID().toString();
-
-        TestAddNameRequest nameRequest = new TestAddNameRequest(name);
-
-        ResponseSpec response = client
-            .post()
-                .uri("/names")
-                .bodyValue(nameRequest)
-            .exchange();
-
-        EntityExchangeResult<TestNameResponse> result = response
-            .expectBody(TestNameResponse.class)
-            .returnResult();
-
-        TestNameResponse nameResponse = result
-                .getResponseBody();
-
-        URI newNameLocation = result
-                .getResponseHeaders().getLocation();
-
-        response.expectStatus().isCreated();
-        assertThat(nameResponse.getName()).isEqualTo(name);
-        assertThat(nameResponse.getId()).isNotNull();
-        assertThat(nameResponse.getId()).isNotEqualTo(new UUID(0, 0));
-        assertThat(newNameLocation.toString()).isEqualTo("http://localhost:" + port + "/names/" + nameResponse.getId());
-	
-        ResponseSpec checkResponse = client
-            .get()
-                .uri(newNameLocation)
-            .exchange();
-
-        checkResponse.expectStatus().isOk();
-
-        TestNameResponse checkNameResponse = checkResponse
-            .expectBody(TestNameResponse.class)
-            .returnResult()
-            .getResponseBody();
-        
-        assertThat(checkNameResponse.getId()).isEqualTo(nameResponse.getId());
-        assertThat(checkNameResponse.getName()).isEqualTo(name);
-    }
 
     @Test
 	public void AddPersonWithFullName() {
